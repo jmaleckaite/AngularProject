57f40380dadb8ed15fc8446a9bd2f2f105def49a
jmaleckaite <justina.maleckaite13@gmail.com>
2020-11-29 20:11:56 +0100

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,14 +11,14 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'Taco recipes';
 
-  private li: any = []
+  private taco: any = []
   
   constructor(private http : HttpClient){}
 
   getData() {
     this.http.get('http://taco-randomizer.herokuapp.com/random/?full-taco=true').subscribe((Response) => {
-    this.li = Response;
-    console.log(this.li)
+    this.taco = Response;
+    console.log(this.taco)
     })
   }
 
